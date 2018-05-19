59da8920e99efa389aec3ce4b9cde1dfe67aea03
Teo <zengwc.teo2016@outlook.com>
2018-05-19 11:47:13 +0800

    init

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,50 @@
+public class Calculator {
+
+    private int num1 = -999999;
+    private int num2 = -999999;
+    private String function = "";
+
+    public Calculator(Number A, Number B) {
+        this.num1 = A.getValue();
+        this.num2 = B.getValue();
+    }
+
+    public Calculator(){}
+
+    public int Add(int A, int B){
+        return A + B;
+    }
+
+    public int Subtract(int A, int B){
+        return A - B;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+    }
+
+    @Override
+    public String toString() {
+        return this.num1 + " + " + this.num2 + " = ";
+    }
+}
